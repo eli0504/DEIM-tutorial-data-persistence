3f975df1be59eb7c52eeeb7bb4ea89112abf5cd5
emesefe <maria@frogames.es>
2024-01-21 19:36:00 +0100

    Base Project

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,38 +1,60 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]
 public class PlayerController : MonoBehaviour
 {
    private Rigidbody2D rigidbody2D;
 
-    private float horizontalInput;
+    private float horizontalInput, verticalInput;
+    private Vector2 inputVector;
     private float moveSpeed = 10f;
-    private float jumpSpeed = 8f;
 
-    private BoxCollider2D boxCollider2D;
-    [SerializeField] private LayerMask groundLayerMask;
+    private int totalCoins;
+
 
     private void Awake()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
         rigidbody2D.constraints = RigidbodyConstraints2D.FreezeRotation;
-
-        boxCollider2D = GetComponentInChildren<BoxCollider2D>();
     }
 
     private void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rigidbody2D.velocity = Vector2.up * jumpSpeed;
-        }
+        inputVector = new Vector2(horizontalInput, verticalInput).normalized;
     }
 
     private void FixedUpdate()
     {
-        rigidbody2D.velocity = new Vector2(moveSpeed * horizontalInput, rigidbody2D.velocity.y);
+        rigidbody2D.velocity = inputVector * moveSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        totalCoins++;
+        Destroy(other.gameObject);
+    }
+
+    public Vector2 GetPosition()
+    {
+        return transform.position;
+    }
+
+    public void SetPosition(Vector2 newPosition)
+    {   
+        transform.position = newPosition;
+    }
+
+    public int GetTotalCoins()
+    {
+        return totalCoins;
+    }
+
+    public void SetTotalCoins(int newTotal)
+    {   
+        totalCoins = newTotal;
     }
 }
